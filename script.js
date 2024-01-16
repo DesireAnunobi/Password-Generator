8d68a152c78908b62a66d3951a30aef125d3b054
DesireAnunobi <desireanunobi@gmail.com>
2024-01-16 23:16:17 +0000

    fixed Function to generate password with user input

--- script.js
+++ script.js
@@ -89,45 +89,60 @@ var upperCasedCharacters = [
 ];
 
 // Function to prompt user for password options
+// Returns undefined
+// Validate input
+// NaN is short for "Not-a-Number". The isNaN() method returns true if a value is NaN.
 function getPasswordOptions() {
   let answers = {};
   let password_length = Number(prompt('How many characters should your password contain? Pick from 8 - 128. characters.'))
+  while(isNaN(password_length)==true || password_length<8 || password_length>128) {
+  password_length = Number(prompt(`Invalid password. Pick a password with 8 - 128 characters. Input the amount of characters`))
 }
 
-// Validate input
-// NaN is short for "Not-a-Number". The isNaN() method returns true if a value is NaN.
-while(isNaN(password_length)==true || password_length<8 || password_length>128) {
-  password_length = prompt(`Invalid password. Pick a password with 8 - 128 characters. Input the amount of characters`)
-};
+return password_length
+
+}
 
 // Password criteria options
-let password_lowerCasedCharacters = confirm(`Will your password include lowercase letters? If No, press Cancel`)
-let password_upperCasedCharacters = confirm(`Will your password include uppercase letters? If No, press Cancel`)
-let password_numericCharacters = confirm(`Will your password include numbers? If No, press Cancel`)
-let password_specialCharacters = confirm(`Will your password include special characters? If No, press Cancel`)
-
-// Answers object
-answers.length = password_length;
-answers.lower = password_lowerCasedCharacters;
-answers.upper = password_upperCasedCharacters;
-answers.numeric = password_numericCharacters;
-answers.special = password_specialCharacters;
 
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-  index = Math.floor(Math.random()*arr.length);
+  var index = Math.floor(Math.random()*arr.length);
   return arr[index];
 }
 
 // Function to generate password with user input
 function generatePassword() {
-  const options = [];
-  const answers = getPasswordOptions();
+  let options = [];
+  var password = ''
+  const password_length = getPasswordOptions();
+  let password_lowerCasedCharacters = confirm(`Will your password include lowercase letters? If No, press Cancel`)
+  let password_upperCasedCharacters = confirm(`Will your password include uppercase letters? If No, press Cancel`)
+  let password_numericCharacters = confirm(`Will your password include numbers? If No, press Cancel`)
+  let password_specialCharacters = confirm(`Will your password include special characters? If No, press Cancel`)
+  if (password_lowerCasedCharacters) {
+    password+=getRandom(lowerCasedCharacters)
+    options=[...options, ...lowerCasedCharacters]
+  }
+  if (password_upperCasedCharacters) {
+    password+=getRandom(upperCasedCharacters)
+    options=[...options, ...upperCasedCharacters]
+  }
+  if (password_numericCharacters) {
+    password+=getRandom(numericCharacters)
+    options=[...options, ...numericCharacters]
+  }
+  if (password_specialCharacters) {
+    password+=getRandom(specialCharacters)
+    options=[...options, ...specialCharacters]
+  }
+  for (let i = password.length; i < password_length; i++) {
+    password+=getRandom(options)
+  }
+  return password
 }
 
-let randomPassword= '';
-for (let i = 0; i < options.length; ++i);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
